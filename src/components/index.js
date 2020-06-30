a1a40f90f07649d91a4435a0c2fd14fd79a20d04
NataliiaRomanenko <nataliia.romanenko@gmail.com>
2020-06-30 12:15:12 +0300

    sortPopup add styles

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Header} from './header';
 export { default as Button} from './button';
 export { default as Categories} from './categories';
+export { default as SortPopup} from './sortPopup';
