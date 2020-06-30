ebe4592d27c65c595a9bd04f45c4118bd76f629d
NataliiaRomanenko <nataliia.romanenko@gmail.com>
2020-06-30 14:54:07 +0300

    add route Home, Cart

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import logoSvg from '../assets/img/pizza-logo.svg';
 import Button from './button'
 
@@ -6,15 +7,19 @@ const Header = () => {
     return(
         <div className="header">
             <div className="container">
-                <div className="header__logo">
-                    <img width="38" src={logoSvg} alt="Pizza logo" />
-                    <div>
-                        <h1>React Pizza</h1>
-                        <p>самая вкусная пицца во вселенной</p>
+                <Link to='/'>
+                    <div className="header__logo">
+                        <img width="38" src={logoSvg} alt="Pizza logo" />
+                        <div>
+                            <h1>React Pizza</h1>
+                            <p>самая вкусная пицца во вселенной</p>
+                        </div>
                     </div>
-                </div>
+                </Link>
+
                 <div className = "header__cart">
-                    <Button className="button--cart">
+                    <Link to="/cart">
+                        <Button className="button--cart">
                         <span>520 ₽</span>
                         <div className="button__delimiter"/>
                         <svg
@@ -47,7 +52,7 @@ const Header = () => {
                         </svg>
                         <span>3</span>
                     </Button>
-
+                    </Link>
                 </div>
             </div>
         </div>
